6887e05dddbe27c1e2c192de77b64e02315002c3
Katarzyna Siwiec <katasiw97@gmail.com>
2019-08-14 12:33:12 +0200

    changing database to json

--- python_exercises/books/utils/datebase.py
+++ python_exercises/books/utils/datebase.py
@@ -1,31 +1,18 @@
-# [x] create an empty list to store books
-# [x] create add book method
-# [x] create list all books method
-# [x] create mark as read
-# [x] create remove a book from books
-
-
-books = []  # list of dictionaries
+import json
 
 
 def add_book(name, author):
-    books.append({'name': name, 'author': author, 'read': False})
+    with open('my_books.txt', 'a') as f:
+        json.dump({'name': name, 'author': author, 'read': False}, f)
 
 
 def list_books():
-    for book in books:
-        print(f"name: {book['name']}")
-        print(f"author: {book['author']}")
-        print(f"read: {str(book['read'])}")
+    pass
 
 
 def mark_as_read(name):
-    for book in books:
-        if name == book['name']:
-            book['read'] = True
+    pass
 
 
 def delete_book(name):
-    for book in books:
-        if name == book['name']:
-            books.remove(book)
+    pass
