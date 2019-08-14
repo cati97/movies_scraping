3deb52d97f3300d04cb6f6e738c5c1fb01918746
Katarzyna Siwiec <katasiw97@gmail.com>
2019-08-14 19:28:53 +0200

    improved delete_book(name) list comprehension

--- python_exercises/books/utils/datebase.py
+++ python_exercises/books/utils/datebase.py
@@ -23,8 +23,16 @@ def mark_as_read(name):
 
 
 def delete_book(name):
+    books_update = [book for book in books if name != book['name']]
+    with open("/home/cati/Desktop/python_exercises/books/utils/my_books.txt", "w") as delete:
+        json.dump(books_update, delete)
+
+
+''' not recommended practice to remove from list while iterating over it
+def delete_book(name):
     for book in books:
         if book['name'] == name:
             books.remove(book)
     with open("/home/cati/Desktop/python_exercises/books/utils/my_books.txt", "w") as delete:
         json.dump(books, delete)
+'''
