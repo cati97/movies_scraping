cc5a2572165db66d2c1a4206ca898ac38b18ed4d
Katarzyna Siwiec <katasiw97@gmail.com>
2019-08-15 19:32:51 +0200

    private method comment, improved create_book_table()

--- python_exercises/books/utils/database_csv.py
+++ python_exercises/books/utils/database_csv.py
@@ -2,7 +2,7 @@ books_file = 'books.txt'
 
 
 def create_book_table():
-    with open(books_file, 'w') as file:
+    with open(books_file, 'a') as file:
         pass  # just to make sure the file is there
 
 
@@ -18,11 +18,11 @@ def get_all_books():
 
 def add_book(name, author):
     with open(books_file, 'a') as file:
-        file.write(f'{name},{author},0\n')
+        file.write(f'{name},{author},NO\n')
 
 
-def _save_all_books(books):
-    with open(books_file, 'w') as file:
+def _save_all_books(books):  # _ means private method - use only within this file
+    with open(books_file, 'w') as file:  # overwrite the file
         for book in books:
             file.write(f"{book['name']},{book['author']},{book['read']}\n")
 
@@ -31,7 +31,7 @@ def mark_book_as_read(name):
     books = get_all_books()
     for book in books:
         if book['name'] == name:
-            book['read'] = '1'
+            book['read'] = 'YES'
     _save_all_books(books)
 
 
